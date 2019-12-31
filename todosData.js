42eb7401a5f185044b2a254b1f1c6de8c9a6686d
Tambi <tambi.asawo@yahoo.com>
2019-12-31 12:54:38 -0800

    Create todosData.js

---
+++ todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Do the dishes",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Go shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Clean gecko tank",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Mow lawn",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Go pick up little sister",
+        completed: false
+    }
+]
+
+export default todosData
